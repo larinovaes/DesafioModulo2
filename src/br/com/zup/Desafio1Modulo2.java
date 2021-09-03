6313082d6d3a4ac4e4328154ca80e49bc980eabc
Larissa Novais <larinovais54@gmail.com>
2021-09-03 13:56:25 -0300

    criando projeto com estrutura HashMap

---
+++ src/br/com/zup/Desafio1Modulo2.java
@@ -0,0 +1,15 @@
+package br.com.zup;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Desafio1Modulo2 {
+
+
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        Map<String, String> funcionarios = new HashMap<String, String>();
+
+    }
+}
